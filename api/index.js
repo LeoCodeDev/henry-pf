2d775635cfe64884417438eb767af4484076bae7
juan manuel montoya giraldo <juanmaggm@gmail.com>
2023-09-25 20:43:05 -0500

    pandorcalculator

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
 function laCajaDePandora(numero) {
   // proximamente escribiremos codigo aqui
+  if (numero % 2 === 0) {
+    return numero.toString(2); // Convierte a binario si es par
+  } else {
+    return numero.toString(16); // Convierte a hexadecimal si es impar
+  }
 }
+
+console.log(laCajaDePandora(17))
