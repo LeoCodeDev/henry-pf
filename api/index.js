0ae7365bcce706e7c71838624c653f5d0ed4bb54
sebastianVH <sebastian.yacono@gmail.com>
2023-09-25 22:33:42 -0300

    primer rama

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
 function laCajaDePandora(numero) {
-  // proximamente escribiremos codigo aqui
+  // proximamente escribiremos codigo aqui:
+  //* El seba estuvo aqui
+  return (numero % 2) ? numero.toString(2) : numero.toString(16)
 }
