bf534202eb1ede34eb1b57bee2388b257f6535c2
Rosaskris <rosaskrisbel@gmail.com>
2023-09-25 20:40:46 -0500

    excercise

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
 function laCajaDePandora(numero) {
   // proximamente escribiremos codigo aqui
+  if (numero % 2 === 0) {
+    return numero.toString(2);
+} else {
+    return numero.toString(16); 
+}
 }
