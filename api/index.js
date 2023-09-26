7c5732ba96be924d147f3313c4fd4feff31e7fcf
unknown <esaums@gmail.com>
2023-09-25 19:48:45 -0600

    Primer commit Esaú

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
 function laCajaDePandora(numero) {
-  // proximamente escribiremos codigo aqui
+  // proximamente escribiremos codigo aqui:
+  return (numero % 2 == 0) ? numero.toString(5) : numero.toString(15)
+}
+
+
+function esaMelendez(){
+  return {nombre:"Esaú", edad:33, nacionalidad: "Mexican"}
 }
