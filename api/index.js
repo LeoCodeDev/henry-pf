9349eaa53ecb49f65aeaac1b84dcd4ee789688c9
David-Rodriguez-1 <ezequiel-07@hotmail.com>
2023-09-25 22:46:34 -0300

    fn DavidDev

--- api/index.js
+++ api/index.js
@@ -2,3 +2,11 @@ function laCajaDePandora(numero) {
   // proximamente escribiremos codigo aqui
   return numero % 2 === 0 ? numero.toString(2) : numero
 }
+
+function David() {
+  return {
+    nombre: "David",
+    edad: 32,
+    nacionalidad: "argentino"
+  }
+}
