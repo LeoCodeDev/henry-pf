ae99171e71bdb414aff36e21424d0b5571c1fac8
juan manuel montoya giraldo <juanmaggm@gmail.com>
2023-09-25 20:46:50 -0500

    funcion mi nombre desarrollada

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 function laCajaDePandora(numero) {
   // proximamente escribiremos codigo aqui
   if (numero % 2 === 0) {
-    return numero.toString(2); // Convierte a binario si es par
+    return numero.toString(2); 
   } else {
-    return numero.toString(16); // Convierte a hexadecimal si es impar
+    return numero.toString(16); 
   }
 }
 
 console.log(laCajaDePandora(17))
+
+let miNombre= ()=>{
+  return miSer= {
+    nombre : 'JUAN MANUEL MONTOYA GIRALDO',
+    nacionalidad : 'Colombiano',
+    edad : 24
+  }
+}
