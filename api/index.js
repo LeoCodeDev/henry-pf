02b38c04dbf5b0da2b0a2a85486b866a0b698042
sebastianVH <sebastian.yacono@gmail.com>
2023-09-25 22:45:14 -0300

    Feature: function w/ name

--- api/index.js
+++ api/index.js
@@ -3,3 +3,8 @@ function laCajaDePandora(numero) {
   //* El seba estuvo aqui
   return (numero % 2 == 0) ? numero.toString(2) : numero.toString(16)
 }
+
+
+function seba(){
+  return {nombre:"Sebastian", edad:36, nacionalidad: "Argentino"}
+}
