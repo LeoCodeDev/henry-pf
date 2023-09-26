744308ea0a23811a91440bc800651507299e2e7b
LeoCodeDev <alb.mar.cta@gmail.com>
2023-09-26 11:47:15 -0500

    Fixing format and adding missing brackets and parenthesis

--- api/index.js
+++ api/index.js
@@ -1,55 +1,54 @@
 function laCajaDePandora(numero) {
-
-if (typeof numero !== 'number' || isNaN(numero)) {
+  if (typeof numero !== "number" || isNaN(numero)) {
     return "Por favor, ingresa un número válido.";
   }
-  
-  return numero % 2 === 0 
-    ? numero.toString(2) 
-    : numero.toString(16);
+
+  return numero % 2 === 0 ? numero.toString(2) : numero.toString(16);
 }
 
-console.log(laCajaDePandora(17)) 
+console.log(laCajaDePandora(17));
 
-const krisFunction=()=>{
+const krisFunction = () => {
   return {
-    name: 'Krisbel Rosas',
+    name: "Krisbel Rosas",
     age: 21,
-    nationality: 'Venezuelan'
-  }
+    nationality: "Venezuelan",
+  };
+};
 
-const leopoldo = ()=>{
-    return {
-        nombre: 'Leopoldo',
-        edad: 35,
-        nacionalidad: "venezolano"
-    }
+const leopoldo = () => {
+  return {
+    nombre: "Leopoldo",
+    edad: 35,
+    nacionalidad: "venezolano",
+  };
+};
+
+function seba() {
+  return { nombre: "Sebastian", edad: 36, nacionalidad: "Argentino" };
 }
-
-function seba(){
-  return {nombre:"Sebastian", edad:36, nacionalidad: "Argentino"}
-
-let miNombre= ()=>{
-  return miSer= {
-    nombre : 'JUAN MANUEL MONTOYA GIRALDO',
-    nacionalidad : 'Colombiano',
-    edad : 24
-  }
-}
+let miNombre = () => {
+  return (miSer = {
+    nombre: "JUAN MANUEL MONTOYA GIRALDO",
+    nacionalidad: "Colombiano",
+    edad: 24,
+  });
+};
 
 function David() {
   return {
     nombre: "David",
     edad: 32,
-    nacionalidad: "argentino"
+    nacionalidad: "argentino",
+  };
 }
-
-function esaMelendez(){
-  return {nombre:"Esaú", edad:33, nacionalidad: "Mexican"}
-
-function holaSoyDiose {
+function esaMelendez() {
+  return { nombre: "Esaú", edad: 33, nacionalidad: "Mexican" };
+}
+function holaSoyDiose() {
   return {
     nombre: "diose",
     edad: 23,
-    nacionalidad: "Venezolana"
-  }
+    nacionalidad: "Venezolana",
+  };
+}
