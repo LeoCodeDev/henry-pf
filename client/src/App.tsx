366f73ed1b2d14411941008cfc32c36599f2d1c1
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-09-27 14:55:24 -0300

    Initiating development of the LandingPage component and resetting App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
+import LandingPage from './components/LandingPage/LandingPage'
 
 const App = (): JSX.Element => {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <LandingPage />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => { setCount((count) => count + 1) }}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h4 className="text-3xl font-bold underline">
-      Este es el grupo de los Elegidos
-    </h4>
     </>
   )
 }
