b18721d99e3379d7bd0ce2019914c78d8499ffe5
juan manuel montoya giraldo <juanmaggm@gmail.com>
2023-09-28 16:34:08 -0500

    putNewPasswordUser creada

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -7,6 +7,8 @@ const getUser= require('../controllers/getUser')
 const postProduct = require('../controllers/postProduct');
 const  getProducts  = require("../controllers/getProducts");
 const  getCategories  = require("../controllers/getCategories");
+const getProductsById = require("../controllers/getProductById");
+const putNewPasswordUser = require("../controllers/PutNewPasswordUser");
 
 router.post('/postTeam', postTeam)
 router.post('/postUser', postUser)
@@ -15,6 +17,8 @@ router.get('/getUser', getUser)
 router.post('/postProduct', postProduct);
 router.get("/products", getProducts);
 router.get("/categories",getCategories );
+router.get("/products/:id", getProducts);
+router.put("/newPassword/:id", putNewPasswordUser);
 
 
 module.exports= router
