489ac489b9a33ea5e8887dcc0332c36d5e61b5a1
juan manuel montoya giraldo <juanmaggm@gmail.com>
2023-09-29 10:09:46 -0500

    correccion de la ruta de putNewPassword y doble verificacion en esta

--- api/src/db_connection.ts
+++ api/src/db_connection.ts
@@ -1,28 +1,27 @@
 require("dotenv").config();
 import { Sequelize } from 'sequelize';
-// const {DB_DEPLOY} = process.env;
-const {DB_USER , DB_PASSWORD , DB_HOST,DBB} = process.env;
+const {DB_DEPLOY} = process.env;
 import {ProductModel} from './models/Product';
 import {CategoryModel} from './models/Category';
 import {UserModel} from './models/User';
 import {TeamModel} from './models/Team';
 
 
-const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DBB}`,
-    { logging: false, native: false }
-)
-// if(!DB_DEPLOY){
-//     throw new Error("DB_DEPLOY is not defined")
+// const sequelize = new Sequelize(
+//     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DBB}`,
+//     { logging: false, native: false }
+// )
+if(!DB_DEPLOY){
+    throw new Error("DB_DEPLOY is not defined")
 
-// }
-// const sequelize = new Sequelize(DB_DEPLOY, 
-//     {logging: false, native: false,
-//         dialectOptions:{
-//         ssl:{
-//         require:true
-//         }
-//     }})
+}
+const sequelize = new Sequelize(DB_DEPLOY, 
+    {logging: false, native: false,
+        dialectOptions:{
+        ssl:{
+        require:true
+        }
+    }})
 
 
 ProductModel(sequelize)
