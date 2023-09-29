cd54510deb67b6886fe9d9293051551d47b90747
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-09-29 14:10:54 -0300

    Completing the LoginPage Component with Login/Signup Functionality and Adding Supplementary Files to Assets and Utils

--- client/src/components/LandingPage/DevOption.jsx
+++ client/src/components/LandingPage/DevOption.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import * as React from "react";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import axios from 'axios';
+///getTeams
 
 export default function SelectLabels(props) {
   const {
@@ -15,9 +16,6 @@ export default function SelectLabels(props) {
     isDesktop,
   } = props;
 
-  const [teams, setTeams] = useState([]); // Estado para almacenar los equipos
-  const [loading, setLoading] = useState(true);
-
   const handleIsDeveloperChange = (event) => {
     const value = event.target.value;
     onIsDeveloperChange(value);
@@ -27,21 +25,6 @@ export default function SelectLabels(props) {
     const value = event.target.value;
     onDeveloperTypeChange(value);
   };
-
-  useEffect(() => {
-    // Realizar la solicitud GET al servidor
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:8000/getTeams'); // Reemplaza '/getTeams' con la ruta correcta
-        setTeams(response.data); // Almacena la respuesta en el estado de los equipos
-        setLoading(false); // Establece loading en false una vez que se ha completado la solicitud
-      } catch (error) {
-        console.error("Error al obtener los equipos:", error);
-      }
-    };
-
-    fetchData();
-  }, []); // El segundo argumento [] asegura que esta solicitud se realice solo una vez al montar el componente
 
   return (
     <div>
@@ -60,7 +43,7 @@ export default function SelectLabels(props) {
         <FormHelperText>Select if you are a developer</FormHelperText>
       </FormControl>
 
-      {isDeveloper === "Yes" && !loading && (
+      {isDeveloper === "Yes" && (
         <FormControl sx={{ ml: isDesktop ? 2 : 0, minWidth: 120, mt: !isDesktop ? 2 : 0 }}>
           <InputLabel id="developer-type-label">Team</InputLabel>
           <Select
@@ -70,11 +53,14 @@ export default function SelectLabels(props) {
             label="Developer Type"
             onChange={handleDeveloperTypeChange}
           >
-            {teams.map((team) => (
-              <MenuItem key={team.id} value={team.name}>
-                {team.name}
-              </MenuItem>
-            ))}
+            <MenuItem value="FullStack">FullStack</MenuItem>
+            <MenuItem value="BackEnd">BackEnd</MenuItem>
+            <MenuItem value="FrontEnd">FrontEnd</MenuItem>
+            <MenuItem value="DataScience">DataScience</MenuItem>
+            <MenuItem value="MobileApp">Mobile App Dev</MenuItem>
+            <MenuItem value="GameDev">Game Developer</MenuItem>
+            <MenuItem value="DevOps">DevOps Engineer</MenuItem>
+            <MenuItem value="AI">AI/ML Engineer</MenuItem>
           </Select>
           <FormHelperText>Select your developer type</FormHelperText>
         </FormControl>
