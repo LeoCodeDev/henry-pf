13f16d03f0645f242013eaf1c806ef0b95ca82e8
LeoCodeDev <alb.mar.cta@gmail.com>
2023-09-30 13:00:47 -0500

    feat(cart): Implement global shopping cart state with Zustand
    - Add functionality to add products to the cart
    - Allow increasing the quantity of products in the cart
    - Allow decreasing the quantity of products in the cart
    - Implement product removal from the cart
    - Add the ability to clear the entire cart
    - Calculate the total per product in the cart
    - Calculate the global total for the purchase
    
    This commit introduces the requested functionalities for managing the shopping cart state using the Zustand library. It includes operations for adding, increasing, decreasing, removing, and clearing the cart, as well as calculating totals per product and the global purchase total.

--- client/src/store/shoppingCartStore.js
+++ client/src/store/shoppingCartStore.js
@@ -1,14 +1,66 @@
-// import { create } from 'zustand'
+import { create } from 'zustand'
 
-// const useCartStore = create((set) => ({
-//   shoppingCart: [],
-//     addProductToCart: (product) => {
-//         set((state)=>({
-//             shoppingCart: [...state.shoppingCart, {
-//                 product,
-//                 quantity,
-//                 total:
-//               }]
-//         }))
-//     }
-// }))
+const useCartStore = create((set) => ({
+  shoppingCart: [],
+  totalToPay: 0,
+  addProductToCart: (product) => {
+    set((state) => ({
+      shoppingCart: [
+        ...state.shoppingCart,
+        {
+          ...product,
+          quantity: 0,
+          total: 0,
+        },
+      ],
+    }))
+  },
+  deleteProductFromCart: (product) => {
+    set((state) => ({
+      shoppingCart: state.shoppingCart.filter((item) => item.id !== product.id),
+    }))
+  },
+  updateProductQuantity: (product, quantity) => {
+    set((state) => ({
+      shoppingCart: state.shoppingCart.map((item) =>
+        item.id === product.id ? (item.quantity = quantity) : item
+      ),
+    }))
+  },
+  clearCart: () => {
+    // eslint-disable-next-line no-unused-vars
+    set((state) => ({
+      shoppingCart: [],
+    }))
+  },
+  oneMore: (product) => {
+    set((state) => ({
+      shoppingCart: state.shoppingCart.map((item) => {
+        product.id === item.id ? item.quantity++ : item
+      }),
+    }))
+  },
+  oneLess: (product) => {
+    set((state) => ({
+      shoppingCart: state.shoppingCart.map((item) => {
+        product.id === item.id ? item.quantity-- : item
+      }),
+    }))
+  },
+  totalByProduct: (product) => {
+    set((state) => ({
+      shoppingCart: state.shoppingCart.map((item) => {
+        product.id === item.id
+          ? (item.total = item.price * item.quantity)
+          : item
+      }),
+    }))
+  },
+  total: () => {
+    set((state) => ({
+      totalToPay: state.shoppingCart.reduce((acc, item) => acc + item.total, 0),
+    }))
+  },
+}))
+
+export { useCartStore }
