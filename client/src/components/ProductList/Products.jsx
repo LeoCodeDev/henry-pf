029a27170627fc04b962cb4cffd5e1c57207fbea
LeoCodeDev <alb.mar.cta@gmail.com>
2023-09-30 21:14:53 -0500

    fix(productStore): Fix bug in fetchProducts and improve state handling
    - The fetchProducts function in the productStore had a bug causing it not to update the state correctly.
    - Improved state handling in the productStore to ensure filteredProducts is populated correctly.
    
    feat(Products): Implement state correctly from productStore
    
    - Fixed the bug in the productStore to ensure fetchProducts works as expected.
    - Implemented state handling in the Products component to display products correctly.
    
    This commit resolves the issue with fetchProducts in the productStore and ensures that the state is correctly implemented in the Products component.

--- client/src/components/ProductList/Products.jsx
+++ client/src/components/ProductList/Products.jsx
@@ -21,55 +21,60 @@ const Products = () => {
 
 export default Products;
  */
-import { useState, useEffect } from "react";
-import { CardProduct } from "../CardProduct/CardProduct";
-import { useProductsStore } from "../../store/productsStore";
-import { IconButton } from "@mui/material";
-import { ArrowBackIosNew, ArrowForwardIos } from "@mui/icons-material";
-import styles from './styles/Products.module.css';
+import { useState, useEffect } from 'react'
+import { CardProduct } from '../CardProduct/CardProduct'
+import { useProductsStore } from '../../store/productsStore'
+import { IconButton } from '@mui/material'
+import { ArrowBackIosNew, ArrowForwardIos } from '@mui/icons-material'
+import styles from './styles/Products.module.css'
 
 const Products = () => {
-  const { fetchProducts, products, filteredProducts } = useProductsStore();
-  const productsPerPage = 8;
-  const [currentPage, setCurrentPage] = useState(0);
-  const totalPages = Math.ceil(products.length / productsPerPage);
+  const { filteredProducts, fetchProducts } = useProductsStore()
+  const productsPerPage = 8
+  const [currentPage, setCurrentPage] = useState(0)
+  const totalPages = Math.ceil(filteredProducts.length / productsPerPage)
 
   useEffect(() => {
-    fetchProducts();
-    console.log('entro a useEffect');
-  }, [filteredProducts, fetchProducts]); 
+    const fetchData = async () => {
+      try {
+        await fetchProducts()
+      } catch (error) {
+        throw new Error(error.message)
+      }
+    }
+    fetchData()
+  }, [fetchProducts])
 
-  console.log({products, filteredProducts});
+  console.log({ filteredProducts })
 
-  const allProducts= filteredProducts.slice(
+  const allProducts = filteredProducts.slice(
     currentPage * productsPerPage,
     (currentPage + 1) * productsPerPage
-  );
+  )
 
   const handleNextPage = () => {
     if (currentPage < totalPages - 1) {
-      setCurrentPage(currentPage + 1);
+      setCurrentPage(currentPage + 1)
     }
-  };
+  }
 
   const handlePrevPage = () => {
     if (currentPage > 0) {
-      setCurrentPage(currentPage - 1);
+      setCurrentPage(currentPage - 1)
     }
-  };
+  }
 
   useEffect(() => {
-    setCurrentPage(0);
-  }, [products]);
+    setCurrentPage(0)
+  }, [filteredProducts])
 
   return (
     <div className={styles.productsContain}>
-  
       <div className={styles.paginationContain}>
         <IconButton
           onClick={handlePrevPage}
           disabled={currentPage === 0}
-          sx={{ color: "#bfbfbf" }}
+          sx={{ color: '#bfbfbf' }}
         >
           <ArrowBackIosNew />
         </IconButton>
@@ -79,7 +84,7 @@ const Products = () => {
         <IconButton
           onClick={handleNextPage}
           disabled={currentPage === totalPages - 1}
-          sx={{ color: "#539a07" }}
+          sx={{ color: '#539a07' }}
         >
           <ArrowForwardIos />
         </IconButton>
@@ -90,9 +95,8 @@ const Products = () => {
           <CardProduct product={product} key={product.id_product} />
         ))}
       </div>
-
     </div>
-  );
-};
+  )
+}
 
-export default Products;
+export default Products
