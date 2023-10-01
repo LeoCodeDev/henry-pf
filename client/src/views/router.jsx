b5b9ecb4d0be42c6ef77b8260bc7223287fb9caf
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-10-01 07:42:24 -0300

    Force path "/" if user is not authenticated

--- client/src/views/router.jsx
+++ client/src/views/router.jsx
@@ -1,7 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import viewsData from './viewsData';
+import { useAuthStore } from "../store/authStore";
+import React, { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Router = () => {
+  const { isLogged } = useAuthStore();
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (!isLogged) {
+      navigate("/");
+    }
+  }, [isLogged]);
+
   const pageRoutes = viewsData.map(({ path, title, element }) => {
     return <Route key={title} path={`/${path}`} element={element} />;
   });
