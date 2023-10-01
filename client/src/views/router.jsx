65c1d3ff974bb844b4e3d225bbc31c01725f0a6d
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-10-01 13:54:28 -0300

    Fixed routes

--- client/src/views/router.jsx
+++ client/src/views/router.jsx
@@ -1,20 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import viewsData from './viewsData';
-import { useAuthStore } from "../store/authStore";
-import React, { useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
-import Error404 from '../components/Error404/Error404'
-import Signup from '../components/LandingPage/SignUp'
+import React from "react";
 
 const Router = () => {
-  const { isLogged } = useAuthStore();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!isLogged && window.location.pathname !== "/signup") {
-      navigate("/");
-    }
-  }, [isLogged, navigate]);
 
   const pageRoutes = viewsData.map(({ path, title, element }) => {
     return <Route key={title} path={`/${path}`} element={element} />;
@@ -22,9 +10,7 @@ const Router = () => {
 
   return (
     <Routes>
-      {pageRoutes}
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/*" element={<Error404 />} />
+      {pageRoutes}s
     </Routes>
   );
 };
