fb6ef3dfd4da71a5acb74889bde06925e76f7e5c
Dioselyn Lemus <105737605+DimeDiose18@users.noreply.github.com>
2023-10-05 13:35:01 -0500

    feat: :sparkles: Creation of the card of both modals, parked for now in the admin view

---
+++ client/src/components/CardProductFav/CardProductFav.jsx
@@ -0,0 +1,56 @@
+import styles from './CardProductFav.module.css';
+import { useState } from 'react';
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+
+export const CardProductFav = ({product}) => {
+    const { name, image, description, price } = product
+    const [isFav, setFav] = useState(false);
+
+  const handleFav = () => {
+    setFav(!isFav);
+    // Falta terminar de hacer los Handlers de fav y cart!
+    // if (isFav) {
+    //   setFav(false);
+    //   removeFav(id)
+    // } else {
+    //   setFav(true);
+    //   addFav
+    // }
+  };
+  return (
+    <>
+      <section className={styles.container}>
+      <div className={styles.imageContainer}>
+        <img src={image} alt={name} />
+      </div>
+
+      <div className={styles.infoContainer}>
+      <h2 className={styles.title}>{name}</h2>
+      <p className={styles.description}>{description}</p>
+      </div>
+
+      <div className={styles.priceContainer}>
+        <h1 className={styles.title}>${price}</h1>
+      </div>
+
+      <div className={styles.stockContain}>
+      <div className={styles.unitsContainer}>
+      <section className={styles.sectionButtons}>
+      {isFav ? (
+              <FavoriteBorderOutlinedIcon
+                fontSize="large"
+                onClick={handleFav}
+              />
+            ) : (
+              <FavoriteOutlinedIcon fontSize="large" onClick={handleFav} />
+            )}
+      </section>
+      </div>
+        
+      </div>
+      </section>
+    </>
+  )
+}
+
