41973c0a1b40ef4c5fbf8ab92b259505e3893481
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-10-07 12:46:50 -0300

    Backend: Adjust handling of user profile updates

--- api/src/controllers/putUser.ts
+++ api/src/controllers/putUser.ts
@@ -1,35 +1,58 @@
 const { User, Team } = require('../db_connection');
-import {Request,Response} from 'express'
+import { Request, Response } from 'express';
 
-const updateUser = async (req:Request, res:Response) => {
-    try {
+const updateUser = async (req: Request, res: Response) => {
+  try {
     const { email, username, first_name, last_name, avatar, team } = req.body;
-    const existingUsername = await User.findOne({ where: { username } });
-    if (existingUsername && existingUsername.email !== email) {
-        return res.status(400).json({ error: 'Username already exists' });
-    }
     const updatedUser = await User.findOne({ where: { email } });
+
     if (!updatedUser) {
-        return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: 'User not found' });
     }
+
+    // Si el nombre de usuario está cambiando
+    if (username !== updatedUser.username) {
+      const existingUserWithUsername = await User.findOne({
+        where: { username },
+      });
+
+      // Verifica si el nuevo nombre de usuario ya está en uso por otro usuario
+      if (existingUserWithUsername && existingUserWithUsername.email !== email) {
+        return res.status(400).json({ error: 'Username already exists' });
+      }
+    }
+
     const updatedUserData = {
-        username,
-        first_name,
-        last_name,
-        avatar,
+      username,
+      first_name,
+      last_name,
+      avatar,
     };
+
     await updatedUser.update(updatedUserData);
+
     if (team) {
-        const teamUpdate = await Team.findOne({ where: { name: team} });
-        if (!teamUpdate) {
+      const teamUpdate = await Team.findOne({ where: { name: team } });
+      console.log(teamUpdate);
+      
+      if (!teamUpdate) {
         return res.status(404).json({ error: 'Team not found' });
-        }
-        await updatedUser.setTeam(teamUpdate);
+      }
+      
+      // Actualiza el equipo del usuario
+      await updatedUser.setTeam(teamUpdate);
     }
-    return res.status(200).json('User updated Succesfully');
-    } catch (error:any) {
+
+    // Devuelve los datos actualizados del usuario en la respuesta
+    const updatedUserWithTeam = await User.findOne({
+      where: { email },
+      include: [{ model: Team, as: 'Team' }],
+    });
+
+    return res.status(200).json(updatedUserWithTeam);
+  } catch (error: any) {
     return res.status(500).json({ error: error.message });
-    }
+  }
 };
 
 module.exports = updateUser;
