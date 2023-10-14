764664343146deadc0641bbf107115da6876b7dd
Dioselyn Lemus <105737605+DimeDiose18@users.noreply.github.com>
2023-10-13 19:42:40 -0500

    feat: :sparkles: Create components for topbar and sidebar

---
+++ client/src/components/AdminView/scenes/Global/TopBar.jsx
@@ -0,0 +1,39 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../themeAdmin";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+export const TopBar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  return (
+    <Box display="flex" justifyContent="space-between" p={2} width='100%'>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
