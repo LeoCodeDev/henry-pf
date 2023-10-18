f4f86b087760748c983fbae1f9e403338751520c
Dioselyn Lemus <105737605+DimeDiose18@users.noreply.github.com>
2023-10-18 14:27:24 -0500

    feat: :sparkles: Create skeleton for view dashboard

--- client/src/components/AdminView/scenes/Dashboard/Dashboard.jsx
+++ client/src/components/AdminView/scenes/Dashboard/Dashboard.jsx
@@ -1,11 +1,34 @@
-
+import { Box, Grid } from "@mui/material";
+import { BasicCard } from "../../../Card/Card";
 
 export const Dashboard = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Box sx={{ m: "1rem" }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={4}>
+            <BasicCard>
 
+            </BasicCard>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <BasicCard>
 
+            </BasicCard>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <BasicCard>
+
+            </BasicCard>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Box sx={{ m: "1rem" }}>
+        <BasicCard customHeight={256}>
+
+        </BasicCard>
+      </Box>
+    </>
+  );
+};
