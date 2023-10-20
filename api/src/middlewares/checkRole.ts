ae755d959b3fb224096d4410be20d427d449c51b
LeoCodeDev <alb.mar.cta@gmail.com>
2023-10-20 18:55:44 -0500

    fixing bugs

--- api/src/middlewares/checkRole.ts
+++ api/src/middlewares/checkRole.ts
@@ -1,43 +1,45 @@
-import { Request, Response, NextFunction } from 'express';
-const { verifyToken } = require('../controllers/JWT');
-const {User} = require('../db_connection');
+import { Request, Response, NextFunction } from 'express'
+const { verifyToken } = require('../controllers/JWT')
+const { User } = require('../db_connection')
 
-const getDetail = async (id : number) => {
-    return await User.findByPk(id)
+const getDetail = async (id: number) => {
+  return await User.findByPk(id)
 }
 
 //TODO: ['user','admin']
-const checkRole = (roles :  string | string[])  => async (req :Request, res : Response, next : NextFunction) => {
+const checkRole =
+  (roles: string | string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const accessToken = req.cookies.accessToken || req.cookies.refreshToken
-        // console.log({aviso : 'aqui estan las cookies' ,req :req.cookies})
-        const verifysing = verifyToken(accessToken);
-        if (!verifysing) {
-            res.status(409)
-            res.send({ error: 'Token invalid' , lugar : 'checkRole'  })
-        } else  {
-            const user = await getDetail(verifysing.id_user);
-            if (user) {
-                 if (Array.isArray(roles) && roles.includes(user.role)) {
-                    next();
-                } else {
-                    // console.log(user)
-                    // console.log(user.role)
+      const trueAccesTokend = req.cookies.accessToken ? true : false
+      const accessToken = req.cookies.accessToken || req.cookies.refreshToken
 
-                    res.status(401);
-                    res.send({ error: 'No tienes permisos' });
-                }
-            }else {
-                res.status(401)
-                res.send({ error: 'No tienes permisos' })
-            }
-        }
-    } catch (e) {
-        console.log('___Error auth___')
+      const verifysing = verifyToken(accessToken, trueAccesTokend)
+      if (!verifysing) {
         res.status(409)
-        res.send({ error: 'Algo sucedio en el middleware authRol' })
-    }
-}
+        res.send({ error: 'Token invalid', lugar: 'checkRole' })
+      } else {
+        const user = await getDetail(verifysing.id_user)
+        if (user) {
+          if (Array.isArray(roles) && roles.includes(user.role)) {
+            next()
+          } else {
+            // console.log(user)
+            // console.log(user.role)
 
+            res.status(401)
+            res.send({ error: 'No tienes permisos' })
+          }
+        } else {
+          res.status(401)
+          res.send({ error: 'No tienes permisos' })
+        }
+      }
+    } catch (e) {
+      console.log('___Error auth___')
+      res.status(409)
+      res.send({ error: 'Algo sucedio en el middleware authRol' })
+    }
+  }
 
 module.exports = checkRole
