64e18e1f5a6a26a09755933520f9e1980da0f17d
sebastianVH <sebastian.yacono@gmail.com>
2023-10-31 11:08:33 -0300

    add top 10

--- api/src/routes/scoreboardRoutes.ts
+++ api/src/routes/scoreboardRoutes.ts
@@ -1,12 +1,14 @@
 import {Router} from 'express'
 import { getUserScoreboard } from '../controllers/scoreboardControllers/getUserScoreboard'
 import { testUserScoreboard } from '../controllers/scoreboardControllers/testUserScoreboard'
+import { getTopScoreboard } from '../controllers/scoreboardControllers/getTopScoreboard'
 
 
 const scoreboardRoutes = Router()
 
 scoreboardRoutes.get('/getUserScores',getUserScoreboard)
 scoreboardRoutes.get('/testUserScore',testUserScoreboard)
+scoreboardRoutes.get('/getTopScores',getTopScoreboard)
 
 
 export default scoreboardRoutes
