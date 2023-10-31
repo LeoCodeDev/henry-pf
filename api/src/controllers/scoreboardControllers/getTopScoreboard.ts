851af009e4ce5ed9b39eeacef041693f6946e4c3
sebastianVH <sebastian.yacono@gmail.com>
2023-10-31 14:15:11 -0300

    Scoreboard features

--- api/src/controllers/scoreboardControllers/getTopScoreboard.ts
+++ api/src/controllers/scoreboardControllers/getTopScoreboard.ts
@@ -1,17 +1,20 @@
-const { User } = require('../../db_connection');
+const { User, Team } = require('../../db_connection');
 import { Response , Request } from "express";
 
 export const getTopScoreboard = async (_req: Request , res: Response) => {
   try {
     const topUsers = await User.findAll({
+      attributes:['id_user','avatar','username','score'],
+      include: {model:Team, attributes: ['name']},
       order: [
         ["score", 'DESC'],
       ],
-      attributes:['id_user','username','score']
+        
     });
-    
     return res.status(200).json(topUsers)
   } catch (error) {
+    console.log(error);
+    
     return res.status(500).json(error);
   }
 }
