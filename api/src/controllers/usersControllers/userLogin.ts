930502d387c23afda15d2abb04ee6b9f6034fd54
sebastianVH <sebastian.yacono@gmail.com>
2023-11-01 13:20:38 -0300

    Seguimos fixeando Main

--- api/src/controllers/usersControllers/userLogin.ts
+++ api/src/controllers/usersControllers/userLogin.ts
@@ -48,7 +48,7 @@ const userLogin = async (req: Request, res: Response) => {
             refreshToken: refreshToken,
     })
   } catch (error: any) {
-    res.status(500).json({ error: error.message })
+    return res.status(500).json({ error: error.message })
   }
 }
 
